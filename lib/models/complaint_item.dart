fc6b098bcc0175bb5f9961e0d4f036a30a6b6ef0
Manasa Thouta <manasathouta@gmail.com>
2024-10-05 02:27:38 +0530

    Enhance ComplaintItem: Added Equatable for comparison and implemented JSON serialization

--- lib/models/complaint_item.dart
+++ lib/models/complaint_item.dart
@@ -1,4 +1,12 @@
-class ComplaintItem {
+import 'package:equatable/equatable.dart';
+
+class ComplaintItem extends Equatable {
+  final String id;
+  final String title;
+  final String description;
+  final String selectedCategory;
+  final String selectedUrgency;
+
   ComplaintItem({
     required this.id,
     required this.title,
@@ -7,9 +15,28 @@ class ComplaintItem {
     required this.selectedUrgency,
   });
 
-  final String id;
-  final String title;
-  final String description;
-  final String selectedCategory;
-  final String selectedUrgency;
+  @override
+  List<Object?> get props => [id, title, description, selectedCategory, selectedUrgency];
+
+  // Factory method to create a ComplaintItem from a JSON object
+  factory ComplaintItem.fromJson(Map<String, dynamic> json) {
+    return ComplaintItem(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      selectedCategory: json['selectedCategory'],
+      selectedUrgency: json['selectedUrgency'],
+    );
+  }
+
+  // Method to convert a ComplaintItem to a JSON object
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'selectedCategory': selectedCategory,
+      'selectedUrgency': selectedUrgency,
+    };
+  }
 }
